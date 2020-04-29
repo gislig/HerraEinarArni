d0813859b0d9743a980891fbd6a5d10e3ceaa326
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2020-04-29 14:57:18 +0000

    smá fix fyrir Herra Einar Árna

---
+++ create-localusers.ps1
@@ -0,0 +1,12 @@
+﻿function main(){
+    if(!(Test-Path users.csv)){
+        Write-Host "Cannot find the file users.csv, are you in the directory where the file is located"
+    }
+    $Users = Import-Csv users.csv -Delimiter ","
+    foreach ($User in $Users) {
+        New-LocalUser -Name $User.notendanafn -Fullname $User.nafn
+        Add-LocalGroupMember -Group $User.hopur -Member $User.notendanafn
+    }
+}
+
+main
